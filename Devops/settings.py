c93f6b52767f31f030073437ec31528a1fc613fd
Jay Vadera <x20129530@student.ncirl.ie>
2020-12-20 19:43:38 +0000

    adde

--- Devops/settings.py
+++ Devops/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'list'
 LOGOUT_REDIRECT_URL = 'list'
